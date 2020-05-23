c3f021d266fca07190f213e5fc37c6273ab08c0b
Arsalan Karimzad <karimzadarsalan@gmail.com>
2020-05-23 19:07:54 +0430

    session 6 finished Register Page created successfully

--- blog/views.py
+++ blog/views.py
@@ -1,32 +1,18 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
 
+
 # Create your views here.
-posts = [
-    {
-        'author': 'arsalan',
-        'title': 'Blog post 1 ',
-        'content': 'First post Content',
-        'date_posted': 'Farvardin',
-
-    },
-    {
-        'author': 'sahand',
-        'title': 'Blog post 2',
-        'content': 'Seceond  post Content',
-        'date_posted': 'Shahrivar',
-
-    }
-]
 
 
 def home(request):
-    contest = {
-        'posts': posts
+    context = {
+        'posts': Post.objects.all()
     }
-    return render(request, 'blog/home.html', contest)
+    return render(request, 'blog/home.html', context)
     pass
 
 
 def about(request):
-    return render(request, 'blog/about.html',{'title':"About"})
+    return render(request, 'blog/about.html', {'title': "About"})
--- youtubeCourse/urls.py
+++ youtubeCourse/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path, include
+from users import views as user_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blog.urls')),
+    path('', include('blog.urls')),
+    path('register/', user_view.register, name="register"),
 
 ]
