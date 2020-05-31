0d29901f79a14f3611adca8b53e8e03e6180d969
Arsalan Karimzad <karimzadarsalan@gmail.com>
2020-05-31 19:18:31 +0430

    reset password email created and work well with activation that I made in my gmail account to send email (access to less secure App Activation)(part 12 Finished )

--- blog/views.py
+++ blog/views.py
@@ -35,7 +35,7 @@ class UserPostListView(ListView):
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
-        return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 
 class PostDetailView(DetailView):  # by default it will look <app>/<model>_<viewtype>.html
--- youtubeCourse/settings.py
+++ youtubeCourse/settings.py
@@ -124,3 +124,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+Email_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "karimzadarsalan@gmail.com"
+EMAIL_HOST_PASSWORD = "arsalankarimzad1234"
